a8b69b9c2945fee7ab8a9f2b5f779e3e2cd54ed3
Larissa Simões <larissacarneiro2@hotmail.com>
2023-10-03 18:39:22 -0300

    create UserService

---
+++ src/main/java/com/paymentgateway/services/UserService.java
@@ -0,0 +1,31 @@
+package com.paymentgateway.services;
+
+import com.paymentgateway.domain.user.User;
+import com.paymentgateway.domain.user.UserType;
+import com.paymentgateway.repositories.UserRepository;
+import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+       if(sender.getUserType() == UserType.MERCHANT){
+         throw new Exception("Usuário do tipo lojista não está autorizado a realizar transação");
+       }
+       if(sender.getBalance().compareTo(amount) < 0){
+         throw new Exception("Saldo insuficiente");
+       }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+      this.repository.save(user);
+    }
+}
